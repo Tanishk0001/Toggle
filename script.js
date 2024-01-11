e815aabd1ef6012ec93a36e56b690687fea9b649
Tanishk0001 <139153225+Tanishk0001@users.noreply.github.com>
2024-01-11 15:25:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let modeBtn = document.querySelector("#btn");
+let body = document.querySelector("body");
+let currentMode = "light";
+modeBtn.addEventListener("click", () => {
+    if(currentMode === "light") {
+        currentMode = "dark";
+        body.classList.add("dark");
+        body.classList.remove("light");
+    }else{
+        currentMode = "light";
+        body.classList.add("light");
+        body.classList.remove("dark");
+    }
+    console.log(currentMode);
+});
+
